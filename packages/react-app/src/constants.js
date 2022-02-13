f1700aae1cb949e81cef0af63124a6a4a92ee001
Nicola Ferracin <nicola.ferracin@gmail.com>
2022-02-13 14:56:46 +0000

    ERC20 Token

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,5 +1,5 @@
 //ToDo: MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "6bfab5bfd0d2475c81e2c61e644a340d";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
 export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
